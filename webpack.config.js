cbfa474e1cb2625f5b904c762224cae95b3d3dc4
safeplace12345 <safeplace12345@gmail.com>
2021-04-05 12:54:30 +0300

    Added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+module.exports = {
+    mode: "development",
+    entry : './src/index.js',
+    output : {
+        filename : "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules:[
+            {
+                test : /\.(png|svg|gif|jpg|jpeg)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
+}
